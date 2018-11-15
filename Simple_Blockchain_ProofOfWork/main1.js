26b783256ea81fa0642e0f2e0f71abd73a55f94f
Miguel Lopez <miguelcodeworld@gmail.com>
2018-11-14 22:24:56 -0500

    Add files via upload

---
+++ Simple_Blockchain_ProofOfWork/main1.js
@@ -0,0 +1,75 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = '')
+    {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: "+ this.hash)
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "13/01/2018", "Genesis block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }                   
+        }
+
+        return true;
+    }   
+}
+
+let mikaeloCoin = new Blockchain();
+
+console.log('Mining block 1...');
+mikaeloCoin.addBlock(new Block(1, "10/07/2018", { amount: 4 }));
+
+console.log('Mining block 2...');
+mikaeloCoin.addBlock(new Block(2, "12/07/2018", { amount: 10 }));
+
+
+console.log(JSON.stringify(mikaeloCoin, null, 4));
+console.log("Is Blockchain valid? " + mikaeloCoin.isChainValid());
